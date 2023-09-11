ad1c59d4b211f8392aad726cd129e0d8c57c6261
student <student@gnu.ac.in>
2023-09-11 10:39:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_7_21012021053/youtubeActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.mad_practical_7_21012021053
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class youtubeActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube2)
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_PRACTICAL_7_21012021053"
+include(":app")
